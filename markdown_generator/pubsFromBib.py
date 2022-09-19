fb325c667d1513dc6495993e3d2addeae61bdd39
Tan-jianbin <70467184+Tan-jianbin@users.noreply.github.com>
2022-09-19 12:37:06 +0800

    Update pubsFromBib.py

--- markdown_generator/pubsFromBib.py
+++ markdown_generator/pubsFromBib.py
@@ -24,25 +24,6 @@ import html
 import os
 import re
 
-#todo: incorporate different collection types rather than a catch all publications, requires other changes to template
-publist = {
-    "proceeding": {
-        "file" : "proceedings.bib",
-        "venuekey": "booktitle",
-        "venue-pretext": "In the proceedings of ",
-        "collection" : {"name":"publications",
-                        "permalink":"/publication/"}
-        
-    },
-    "journal":{
-        "file": "pubs.bib",
-        "venuekey" : "journal",
-        "venue-pretext" : "",
-        "collection" : {"name":"publications",
-                        "permalink":"/publication/"}
-    } 
-}
-
 html_escape_table = {
     "&": "&amp;",
     '"': "&quot;",
