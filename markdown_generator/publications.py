2c226392515e19b1dbb44914bc264a60faf4f276
Tan-jianbin <70467184+Tan-jianbin@users.noreply.github.com>
2022-09-19 12:26:54 +0800

    Update publications.py

--- markdown_generator/publications.py
+++ markdown_generator/publications.py
@@ -79,11 +79,6 @@ for row, item in publications.iterrows():
     if len(str(item.excerpt)) > 5:
         md += "\nexcerpt: '" + html_escape(item.excerpt) + "'"
     
-    if len(str(item.pub_date)) > 1:
-    md += "\ndate: " + str(item.pub_date) 
-    
-    if len(str(item.venue)) > 1:
-    md += "\nvenue: '" + html_escape(item.venue) + "'"
     
     if len(str(item.paper_url)) > 5:
         md += "\npaperurl: '" + item.paper_url + "'"
